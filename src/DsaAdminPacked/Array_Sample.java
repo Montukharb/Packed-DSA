7fe1124fc0cdf5abf1271df2c57cb8ea5ed5afa5
Montukharb <montukharb868@gmail.com>
2024-10-08 23:46:24 +0530

    Array All Operation

--- src/DsaAdminPacked/Array_Sample.java
+++ src/DsaAdminPacked/Array_Sample.java
@@ -18,8 +18,13 @@ public class Array_Sample {
              elements[i] = sc.nextInt();
         }
         create.DisplayArray(elements,size);
+        create.AddFirst(elements,size);
+        create.AddEnd(elements,size);
+        create.AddPosition(elements,size);
+
 
     }
+
 }
 class Arrays{
     public void DisplayArray(int []elements,int size)
@@ -27,6 +32,79 @@ class Arrays{
         for (int i = 0;i<size;i++)
         {
             System.out.print(elements[i]+" ");
+        }
+    }
+    public void AddFirst(int []elements,int size)
+    {
+        System.out.println("Enter new element");
+        Scanner sc = new Scanner(System.in);
+        int newElement = sc.nextInt();
+        int []b = new int[size+1];
+        for(int i=0;i<size+1;i++)
+        {
+            if(i==0)
+            {
+                b[i]=newElement;
+            }
+            else
+            {
+                b[i]=elements[i-1];
+            }
+        }
+        for (int e:b)
+        {
+            System.out.println(e+" ");
+        }
+    }
+    public void AddEnd(int []array,int size)
+    {
+        System.out.println("Enter new elements");
+        Scanner sc = new Scanner(System.in);
+        int newElement = sc.nextInt();
+        int []b = new int[size+1];
+        for(int i=0;i<size+1;i++)
+        {
+            if(i<size)
+            {
+                b[i]=array[i];
+            }
+            else
+            {
+                b[i]=newElement;
+            }
+        }
+        for (int e:b)
+        {
+            System.out.println("After addition End"+e+" ");
+        }
+    }
+    public void AddPosition(int []array,int size)
+    {
+        System.out.println("Enter position");
+        int []b = new int[size+1];
+        Scanner sc = new Scanner(System.in);
+        int pos = sc.nextInt();
+        System.out.println("Enter new element");
+        int new_element = sc.nextInt();
+        for(int i=0;i<size+1;i++)
+        {
+            if(i<pos)
+            {
+                b[i] = array[i];
+            }
+            else if(i==pos)
+            {
+                b[i]=new_element;
+            }
+            else
+            {
+                b[i]=array[i-1];
+            }
+        }
+        System.out.println("After position");
+        for (int c:b)
+        {
+            System.out.println(c+" ");
         }
     }
 }
