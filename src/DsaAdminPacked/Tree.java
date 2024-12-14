752ed96bc1fb9f7b4adf09a253f252ad799ac017
Montukharb <montukharb868@gmail.com>
2024-12-14 15:03:25 +0530

    new tree

--- src/DsaAdminPacked/Tree.java
+++ src/DsaAdminPacked/Tree.java
@@ -4,5 +4,6 @@ public class Tree {
     public static void main(String []args)
     {
         System.out.println("Create binary updated tree data structure");
+        System.out.println("new tree");
     }
 }
